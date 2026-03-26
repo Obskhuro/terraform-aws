646dd89fe1ec8917ef5a011314d09fdd86fe2dd6
Obskhuro <yanisg2606@gmail.com>
2026-03-26 10:14:42 +0100

    tp combiné

--- security_groups.tf
+++ security_groups.tf
@@ -8,18 +8,13 @@ resource "aws_security_group" "grousselle_ec2_sg" {
   vpc_id      = data.aws_vpc.default.id
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+
 
   egress {
     from_port   = 0
